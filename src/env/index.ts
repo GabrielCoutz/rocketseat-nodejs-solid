7d93c51ecc08f7a35f10a3a4af895db2df9e1840
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-02-23 21:11:07 -0300

    feat: integrate Prisma for database management and add user, gym, and check-in models
    - Updated package.json to include Prisma dependencies and added a script for Prisma Studio.
    - Created prisma.config.ts for Prisma configuration.
    - Added migration scripts for creating users, gyms, and check-ins tables with necessary relationships.
    - Defined Prisma schema with User, CheckIn, and Gym models.
    - Updated environment variable validation to include DATABASE_URL.

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,12 @@ import z from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+  DATABASE_URL: z.string(),
 })
 
 export const _env = envSchema.safeParse(process.env)
 
-if(!_env.success) {
+if (!_env.success) {
   console.error('Invalid environment variables', _env.error.format())
   throw new Error('Invalid environment variables')
 }
